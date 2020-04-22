98127ea2f5b16a50f6718a2679304ffe1a73327c
yuanzhicheng <1798788388@qq.com>
2020-04-22 11:11:10 +0800

    ddd

--- .umirc.js
+++ .umirc.js
@@ -8,5 +8,12 @@ export default {
                 exclude:[/.*\/models\/.*/,/.*\/model\.js/]
             }
         }]
-    ]
+    ],
+    proxy:{//相当于webpack的devServer中的proxy配置
+        // "/api":{
+        //     target:"http://api.duyiedu.com",
+        //     changeOrigin:true//修改源
+        // }
+
+    }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 /**
  * title: 首页
  */
+import {getAllStudents} from "@/services/student.js"
 import React from 'react'
 
 export default function index() {
+    getAllStudents().then(res => {
+        console.log(res);
+    })
     return (
         <div>
             <h1>首页1 </h1>
